16108984d328737e2228e92d085d47acb5d0170d
nokoi <63418160+studioNOKOI@users.noreply.github.com>
2020-07-10 21:19:03 -0400

    jumping
    it's here

--- GMTKJam2020/Assets/scripts/movement.cs
+++ GMTKJam2020/Assets/scripts/movement.cs
@@ -4,13 +4,31 @@ using UnityEngine;
 
 public class movement : MonoBehaviour
 {
+    [Header("Movement")]
     public float moveSpd;
     public float moveLimit;
     public float maxMoveRight;
     public float maxMoveLeft;
+    [Header("Jumping")]
+    public bool onGround;
+    public float jSpd;
+    public Rigidbody rb;
+    public int jumpJuice; // stupid name but it is how many frames you can hold jump for before you stop ascending
+    public float gravScale;
+    public static float Gravity = -9.81f;
+    [Header("Raycast Stuffs")]
+    public float raycastLength;
+    public LayerMask ground;
     void Update()
     {
         move();
+        jump();
+        jumpDetectionRaycast();
+    }
+    void FixedUpdate()
+    {
+        Vector3 gravity = Gravity * gravScale * Vector3.up; // gravity stuffs
+        rb.AddForce(gravity, ForceMode.Acceleration); // adding the gravity
     }
     void move()
     {
@@ -25,6 +43,31 @@ public class movement : MonoBehaviour
             moveLimit -= 1; // subtracts 1 from the moveLimit every frame left is held (basically limits how far left char can go)
             transform.Translate(new Vector2(-moveSpd * Time.deltaTime, 0)); // translate by moveSpd to the left
             transform.localScale = (new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z)); // sets char facing left
+        }
+    }
+    void jump()
+    {
+        jumpJuice -= 1; // subtract jump juice every frame
+        if (Input.GetButton("Jump") && jumpJuice > 0) // if you press jump and have jump juice still
+        {
+            rb.velocity = new Vector3(rb.velocity.x, jSpd, rb.velocity.z); // set jump velocity
+        }
+        if (Input.GetButtonUp("Jump")) // when you release jump
+        {
+            jumpJuice = 0; // no more jump juice
+        }
+        if (jumpJuice <= 0) // if you have no jump juice left
+        {
+            gravScale = 4; // more gravity for faster fall (feels good i swear)
+        }
+
+    }
+    void jumpDetectionRaycast()
+    {
+        if (Physics.Raycast(transform.position, Vector3.down, raycastLength, ground)) // casts a ray down of raycastLength distance that only returns info if it hits ground
+        {
+            gravScale = 1; // normal gravity
+            jumpJuice = 30; // you are on ground so you have full juice
         }
     }
 }
