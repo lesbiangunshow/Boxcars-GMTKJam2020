019a650d4e8b67b246c7e0cc99405980967e498c
nokoi <63418160+studioNOKOI@users.noreply.github.com>
2020-07-11 00:48:21 -0400

    random ball spawning
    the groundwork to add more items is there i just gotta make collisions and stuff that happens if you hit the items

---
+++ GMTKJam2020/Assets/scripts/spawnItem.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnItem : MonoBehaviour
+{
+    public GameObject[] items;
+    public int whichItem;
+    public int timerUntilNextItem;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        timerUntilNextItem -= 1;
+        if (timerUntilNextItem < 0)
+        {
+            itemSpawn();
+        }
+    }
+    void itemSpawn()
+    {
+        whichItem = Random.Range(0, 2); // choose randomly from a range which item to spawn (change 2nd range value for each item added)
+        GameObject item = Instantiate(items[whichItem]); // spawn the item
+        item.transform.position = transform.position; // set position of item to item shooter location
+        timerUntilNextItem = Random.Range(300, 540);
+    }
+}
